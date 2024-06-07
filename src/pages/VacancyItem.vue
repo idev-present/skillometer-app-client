7f7c4fa61498188c7e817d675ffcd503f69a487e
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-07 10:58:12 +0300

    feat(app): vacancyItem

--- src/pages/VacancyItem.vue
+++ src/pages/VacancyItem.vue
@@ -2,10 +2,10 @@
   <div class="mx-auto w-full max-w-7xl mt-[88px] px-4 py-8 md:py-16 sm:px-6 lg:px-8">
     <div>
       <nav class="sm:hidden" aria-label="Back">
-        <a href="#" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">
+        <router-link to="/vacancy" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">
           <ChevronLeftIcon class="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
           Back
-        </a>
+        </router-link>
       </nav>
       <nav class="hidden sm:flex" aria-label="Breadcrumb">
         <ol role="list" class="flex items-center space-x-4">
