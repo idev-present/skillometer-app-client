11e712b185b04b303273171fb7080aa78ad0375b
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-07 21:06:14 +0300

    feat(app): link

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -35,9 +35,9 @@
         <div class="fixed inset-0 z-50" />
         <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
-            <a href="#" class="-mx-1.5 -mt-2 p-1.5">
+            <router-link to="/" class="-mx-1.5 -mt-2 p-1.5">
               <img class="h-8 w-auto" src="https://www.reksoft.ru/wp-content/uploads/2019/05/logo.png" alt="" />
-            </a>
+            </router-link>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
               <XMarkIcon class="h-6 w-6" aria-hidden="true" />
             </button>
