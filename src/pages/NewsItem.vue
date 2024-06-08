0977367b17dc61809bf0ded7cf93ad573842d974
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-08 19:06:18 +0300

    feat(app: News

--- src/pages/NewsItem.vue
+++ src/pages/NewsItem.vue
@@ -3,8 +3,13 @@
     <nav class="sm:hidden" aria-label="Back">
       <router-link
         to="/news"
-        class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">
-        <ChevronLeftIcon class="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />Назад</router-link>
+        class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+      >
+        <ChevronLeftIcon
+          class="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400"
+          aria-hidden="true"
+        />Назад</router-link
+      >
     </nav>
     <nav class="hidden sm:flex" aria-label="Breadcrumb">
       <ol role="list" class="flex items-center space-x-2">
@@ -12,13 +17,20 @@
           <div class="flex">
             <router-link
               to="/news"
-              class="text-sm font-medium text-gray-500 hover:text-gray-700">Новости</router-link>
+              class="text-sm font-medium text-gray-500 hover:text-gray-700"
+              >Новости</router-link
+            >
           </div>
         </li>
         <li>
           <div class="flex items-center">
-            <ChevronRightIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-            <span class="ml-2 text-sm font-medium text-gray-700">{{news.title}}</span>
+            <ChevronRightIcon
+              class="h-5 w-5 flex-shrink-0 text-gray-400"
+              aria-hidden="true"
+            />
+            <span class="ml-2 text-sm font-medium text-gray-700">{{
+              news.title
+            }}</span>
           </div>
         </li>
       </ol>
@@ -155,11 +167,101 @@
         </p>
       </div>
     </div>
-    
+
     <!-- Comments -->
     <div class="mt-4 bg-white py-4 px-4 sm:px-6 sm:py-6 rounded-xl shadow-sm ring-1 ring-gray-900/5">
       <h2 class="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight border-b-2 pb-3">Комментарии</h2>
-    
+      <ul role="list" class="space-y-4 mt-4">
+        <li
+          v-for="question in questions"
+          :key="question.id"
+          class="bg-white px-4 py-6 shadow-sm ring-1 ring-gray-900/5 sm:p-6 rounded-xl"
+        >
+          <article :aria-labelledby="'question-title-' + question.id">
+            <div>
+              <div class="flex space-x-3">
+                <div class="flex-shrink-0">
+                  <img
+                    class="h-10 w-10 rounded-full"
+                    :src="question.author.imageUrl"
+                    alt=""
+                  />
+                </div>
+                <div class="min-w-0 flex-1">
+                  <p class="text-sm font-medium text-gray-900"><a :href="question.author.href" class="hover:underline">{{question.author.name}}</a></p>
+                  <p class="text-sm text-gray-500">
+                    <a :href="question.href" class="hover:underline">
+                      <time :datetime="question.datetime">{{question.date}}</time>
+                    </a>
+                  </p>
+                </div>
+                <div class="flex flex-shrink-0 self-center"></div>
+              </div>
+              <h2
+                :id="'question-title-' + question.id"
+                class="mt-4 text-base font-medium text-gray-900">{{ question.title }}</h2>
+            </div>
+            <div
+              class="mt-2 space-y-4 text-sm text-gray-700"
+              v-html="question.body"
+            />
+            <div class="mt-6 flex justify-between space-x-8">
+              <div class="flex space-x-6">
+                <span class="inline-flex items-center text-sm">
+                  <button
+                    type="button"
+                    class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                  >
+                    <HandThumbUpIcon class="h-5 w-5" aria-hidden="true" />
+                    <span class="font-medium text-gray-900">{{
+                      question.likes
+                    }}</span>
+                    <span class="sr-only">likes</span>
+                  </button>
+                </span>
+                <span class="inline-flex items-center text-sm">
+                  <button
+                    type="button"
+                    class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                  >
+                    <ChatBubbleLeftEllipsisIcon
+                      class="h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    <span class="font-medium text-gray-900">{{
+                      question.replies
+                    }}</span>
+                    <span class="sr-only">replies</span>
+                  </button>
+                </span>
+                <span class="inline-flex items-center text-sm">
+                  <button
+                    type="button"
+                    class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                  >
+                    <EyeIcon class="h-5 w-5" aria-hidden="true" />
+                    <span class="font-medium text-gray-900">{{
+                      question.views
+                    }}</span>
+                    <span class="sr-only">views</span>
+                  </button>
+                </span>
+              </div>
+              <div class="flex text-sm">
+                <span class="inline-flex items-center text-sm">
+                  <button
+                    type="button"
+                    class="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
+                  >
+                    <ShareIcon class="h-5 w-5" aria-hidden="true" />
+                    <span class="font-medium text-gray-900">Share</span>
+                  </button>
+                </span>
+              </div>
+            </div>
+          </article>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -167,29 +269,60 @@
 <script setup>
 import { ref } from "vue";
 import {
+  ChatBubbleLeftEllipsisIcon,
+  EyeIcon,
+  HandThumbUpIcon,
+  ShareIcon,
+} from "@heroicons/vue/20/solid";
+import {
   ChevronRightIcon,
-  BriefcaseIcon,
-  MapPinIcon,
-  CalendarIcon,
   ChevronLeftIcon,
   CheckCircleIcon,
 } from "@heroicons/vue/20/solid";
 
 const news = ref({
-    title: "Обзор изменений в законодательстве за май 2024 года",
-    href: "#",
-    description:
-      "В обзоре изменений за май 2024 года рассмотрим следующие темы: критическая информационная инфраструктура, безопасность финансовых организаций и другие важные изменения в законодательстве в сфере ИТ и ИБ.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-    date: "Mar 16, 2020",
-    time: "10:20",
+  title: "Обзор изменений в законодательстве за май 2024 года",
+});
+const questions = [
+  {
+    id: "1",
+    likes: "29",
+    replies: "11",
+    views: "2.7k",
     author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
+      name: "Dries Vincent",
       imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      href: "#",
     },
-  });
+    date: "December 9 at 11:43 AM",
+    datetime: "2020-12-09T11:43:00",
+    href: "#",
+    title: "What would you have done differently if you ran Jurassic Park?",
+    body: `
+      <p>Jurassic Park was an incredible idea and a magnificent feat of engineering, but poor protocols and a disregard for human safety killed what could have otherwise been one of the best businesses of our generation.</p>
+      <p>Ultimately, I think that if you wanted to run the park successfully and keep visitors safe, the most important thing to prioritize would be&hellip;</p>
+    `,
+  },
+  {
+    id: "2",
+    likes: "29",
+    replies: "11",
+    views: "2.7k",
+    author: {
+      name: "Dries Vincent",
+      imageUrl:
+        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      href: "#",
+    },
+    date: "December 9 at 11:43 AM",
+    datetime: "2020-12-09T11:43:00",
+    href: "#",
+    title: "What would you have done differently if you ran Jurassic Park?",
+    body: `
+      <p>Jurassic Park was an incredible idea and a magnificent feat of engineering, but poor protocols and a disregard for human safety killed what could have otherwise been one of the best businesses of our generation.</p>
+      <p>Ultimately, I think that if you wanted to run the park successfully and keep visitors safe, the most important thing to prioritize would be&hellip;</p>
+    `,
+  },
+];
 </script>
