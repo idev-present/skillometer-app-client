263a687fdd85c253d2e224d5591314146e0db918
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-13 18:13:10 +0300

    feat(app): search input

--- src/widgets/ListFilters.vue
+++ src/widgets/ListFilters.vue
@@ -16,59 +16,75 @@
     <div v-if="isShowFilters" class="p-4 pt-0 lg:p-0">
       <!--    qualification-->
       <div class="w-full lg:mt-4">
-        <Listbox as="div" v-model="selectedQualification">
-          <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900">
+        <Combobox
+            as="div"
+            v-model="selectedQualification"
+            @update:modelValue="queryQualification = ''"
+        >
+          <ComboboxLabel class="block text-sm font-medium leading-6 text-gray-900">
             Квалификация
-          </ListboxLabel>
+          </ComboboxLabel>
           <div class="relative mt-0.5">
-            <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
-              <span class="block truncate">{{ selectedQualification.name }}</span>
-              <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </span>
-            </ListboxButton>
-            <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-              <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                <ListboxOption as="template" v-for="(item, index) in qualificationList" :key="index" :value="item" v-slot="{ active, selected }">
-                  <li :class="[active ? 'bg-blue-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
-                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ item.name }}</span>
-                    <span v-if="selected" :class="[active ? 'text-white' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
-                <CheckIcon class="h-5 w-5" aria-hidden="true" />
-              </span>
-                  </li>
-                </ListboxOption>
-              </ListboxOptions>
-            </transition>
+            <ComboboxButton class="relative w-full">
+              <ComboboxInput class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-8 text-gray-900 shadow-sm ring-1 outline-0 ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                             :display-value="(item) => item?.name || item"
+                             @change="queryQualification = $event.target.value"
+                             @blur="queryQualification = ''"
+              />
+              <div class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </div>
+            </ComboboxButton>
+            <ComboboxOptions v-if="filteredQualification?.length > 0" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <ComboboxOption v-for="(item, index) in filteredQualification" :key="index" :value="item" as="template" v-slot="{ active, selected }">
+                <li :class="['relative cursor-default select-none py-2 pl-3 pr-9', active ? 'bg-blue-600 text-white' : 'text-gray-900']">
+                  <span :class="['block truncate', selected && 'font-semibold']">
+                    {{ item.name }}
+                  </span>
+                  <span v-if="selected" :class="['absolute inset-y-0 right-0 flex items-center pr-4', active ? 'text-white' : 'text-blue-600']">
+                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </li>
+              </ComboboxOption>
+            </ComboboxOptions>
           </div>
-        </Listbox>
+        </Combobox>
       </div>
       <!--  skills  -->
       <div class="w-full mt-4">
-        <Listbox as="div" v-model="selectedDivision">
-          <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900">
+        <Combobox
+            as="div"
+            v-model="selectedDivision"
+            @update:modelValue="queryDivision = ''"
+        >
+          <ComboboxLabel class="block text-sm font-medium leading-6 text-gray-900">
             Специализация
-          </ListboxLabel>
+          </ComboboxLabel>
           <div class="relative mt-0.5">
-            <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
-              <span class="block truncate">{{ selectedDivision.name }}</span>
-              <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </span>
-            </ListboxButton>
-            <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-              <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                <ListboxOption as="template" v-for="(item, index) in divisionList" :key="index" :value="item" v-slot="{ active, selected }">
-                  <li :class="[active ? 'bg-blue-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
-                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ item.name }}</span>
-                    <span v-if="selected" :class="[active ? 'text-white' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
-                <CheckIcon class="h-5 w-5" aria-hidden="true" />
-              </span>
-                  </li>
-                </ListboxOption>
-              </ListboxOptions>
-            </transition>
+            <ComboboxButton class="relative w-full">
+              <ComboboxInput class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-8 text-gray-900 shadow-sm ring-1 outline-0 ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                             :display-value="(item) => item?.name || item"
+                             @change="queryDivision = $event.target.value"
+                             @blur="queryDivision = ''"
+              />
+              <div class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </div>
+            </ComboboxButton>
+            <ComboboxOptions v-if="filteredDivision?.length > 0" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <ComboboxOption v-for="(item, index) in filteredDivision" :key="index" :value="item" as="template" v-slot="{ active, selected }">
+                <li :class="['relative cursor-default select-none py-2 pl-3 pr-9', active ? 'bg-blue-600 text-white' : 'text-gray-900']">
+                  <span :class="['block truncate', selected && 'font-semibold']">
+                    {{ item.name }}
+                  </span>
+                  <span v-if="selected" :class="['absolute inset-y-0 right-0 flex items-center pr-4', active ? 'text-white' : 'text-blue-600']">
+                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </li>
+              </ComboboxOption>
+            </ComboboxOptions>
           </div>
-        </Listbox>
+        </Combobox>
       </div>
       <!--  Employment type  -->
       <div class="w-full mt-4">
@@ -140,7 +156,17 @@
 
 <script setup>
 import {ref, computed, onMounted} from 'vue'
-import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import {
+  Combobox,
+  ComboboxLabel,
+  ComboboxButton,
+  ComboboxInput, ComboboxOption, ComboboxOptions,
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions
+} from '@headlessui/vue'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
 import {ChevronRightIcon} from "@heroicons/vue/20/solid/index.js";
 import {useDirectoriesStore} from "@/app/store/modules/directories.js";
@@ -173,7 +199,23 @@ const divisionList = computed(() => {
   ]
 })
 const inputValue = ref('')
+const queryQualification = ref('')
+const queryDivision = ref('')
 
+const filteredQualification = computed(() => {
+  return queryQualification.value === ''
+      ? qualificationList.value
+      : qualificationList?.value?.filter((item) => {
+        return item.name.toLowerCase().includes(queryQualification.value.toLowerCase())
+  }) || []
+})
+const filteredDivision = computed(() => {
+  return queryDivision.value === ''
+      ? divisionList.value
+      : divisionList?.value?.filter((item) => {
+    return item.name.toLowerCase().includes(queryDivision.value.toLowerCase())
+  }) || []
+})
 
 const inputPrice = (value) => {
   const regex = /^(\d+(\.\d{0,2})?)?$/;
