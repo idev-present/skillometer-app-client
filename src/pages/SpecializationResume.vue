d0fbd60f922dfce105a36e922a6682988b1285a8
Igor Zhuravlev <igor.zhuravlev@idev-present.com>
2024-06-15 13:25:54 +0300

    feat(app): Education input

--- src/pages/SpecializationResume.vue
+++ src/pages/SpecializationResume.vue
@@ -97,37 +97,43 @@
                       </span>
                     </div>
                   </div>
+                  
                   <!--Специализация/зп-->
                   <div class="mt-4 grid grid-cols-12 gap-4 sm:gap-6">
                     <div class="col-span-12 sm:col-span-6">
-                      <Listbox as="div" v-model="user.selectedDivision">
-                        <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900">
-                          Специализация
-                        </ListboxLabel>
-                        <div class="relative mt-0.5">
-                          <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
-                            <span class="block truncate">{{ user?.selectedDivision?.name || '&nbsp;' }}</span>
-                            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <Combobox
+                        as="div"
+                        v-model="user.selectedDivision"
+                        @update:modelValue="queryDivision = ''"
+                      >
+                      <ComboboxLabel class="block text-sm font-medium leading-6 text-gray-900">
+                        Специализация
+                      </ComboboxLabel>
+                      <div class="relative mt-0.5">
+                        <ComboboxButton class="relative w-full">
+                          <ComboboxInput class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-8 text-gray-900 shadow-sm ring-1 outline-0 ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                            :display-value="(item) => item?.name || item"
+                            @change="queryDivision = $event.target.value"
+                            @blur="queryDivision = ''"
+                          />
+                          <div class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
                             <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                          </span>
-                          </ListboxButton>
-                          <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
-                            <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                              <ListboxOption as="template" v-for="(item, index) in divisionList" :key="index" :value="item" v-slot="{ active, selected }">
-                                <li :class="[active ? 'bg-blue-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
-                                  <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ item.name }}</span>
-                                  <span v-if="selected" :class="[active ? 'text-white' : 'text-blue-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
-                                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                                </span>
-                                </li>
-                              </ListboxOption>
-                            </ListboxOptions>
-                          </transition>
-                        </div>
-                      </Listbox>
-                      <span v-if="errors?.division_id" class="text-red-600 text-sm">
-                        {{errors.division_id}}
-                      </span>
+                          </div>
+                        </ComboboxButton>
+                        <ComboboxOptions v-if="filteredDivision?.length > 0" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          <ComboboxOption v-for="(item, index) in filteredDivision" :key="index" :value="item" as="template" v-slot="{ active, selected }">
+                            <li :class="['relative cursor-default select-none py-2 pl-3 pr-9', active ? 'bg-blue-600 text-white' : 'text-gray-900']">
+                              <span :class="['block truncate', selected && 'font-semibold']">
+                                {{ item.name }}
+                              </span>
+                              <span v-if="selected" :class="['absolute inset-y-0 right-0 flex items-center pr-4', active ? 'text-white' : 'text-blue-600']">
+                                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            </li>
+                          </ComboboxOption>
+                        </ComboboxOptions>
+                      </div>
+                      </Combobox>
                     </div>
 
                     <div class="col-span-12 sm:col-span-6">
@@ -257,6 +263,10 @@ import Loading from "@/shared/Loading.vue";
 import {
   Listbox,
   ListboxButton, ListboxLabel, ListboxOption, ListboxOptions,
+  Combobox,
+  ComboboxLabel,
+  ComboboxButton,
+  ComboboxInput, ComboboxOption, ComboboxOptions,
 } from '@headlessui/vue'
 import {
   AcademicCapIcon,
@@ -306,6 +316,8 @@ const errors = ref({
   skill_set: '',
 })
 
+const queryDivision = ref('')
+
 const searchStatusList = computed(() => {
   return directoriesStore?.searchStatusList || []
 })
@@ -320,6 +332,14 @@ const skillList = computed(() => {
 })
 const divisionList = computed(() => {
   return directoriesStore?.divisionList || []
+})
+
+const filteredDivision = computed(() => {
+  return queryDivision.value === ''
+      ? divisionList.value
+      : divisionList?.value?.filter((item) => {
+    return item.name.toLowerCase().includes(queryDivision.value.toLowerCase())
+  }) || []
 })
 
 const inputPrice = (event) => {
